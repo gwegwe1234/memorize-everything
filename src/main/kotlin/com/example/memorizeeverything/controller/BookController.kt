4d0764b8651b2e1e843b498aa1c331766acde0b0
gwegwe1234 <gwangtae5860@daum.net>
2023-02-19 23:01:07 +0900

    도커 설정 변경

--- src/main/kotlin/com/example/memorizeeverything/controller/BookController.kt
+++ src/main/kotlin/com/example/memorizeeverything/controller/BookController.kt
@@ -2,15 +2,23 @@ package com.example.memorizeeverything.controller
 
 import com.example.memorizeeverything.model.book.Book
 import com.example.memorizeeverything.service.BookService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/book")
 class BookController(private val bookService: BookService) {
 
-    @GetMapping("/book-info")
-    fun getBookInfo(
+    @GetMapping("")
+    fun getBookInfo() : ResponseEntity<List<Book>> {
+        return ResponseEntity.ok(bookService.findAll())
+    }
+
+    @GetMapping("/info")
+    fun getBookInfoByTitle(
         @RequestParam("title") title: String
     ): Book {
         return bookService.findAllByTitle(title)
--- src/main/kotlin/com/example/memorizeeverything/service/BookService.kt
+++ src/main/kotlin/com/example/memorizeeverything/service/BookService.kt
@@ -7,7 +7,13 @@ import org.springframework.stereotype.Component
 @Component
 class BookService(private val bookRepository: BookRepository) {
 
+    fun findAll(): List<Book>? {
+        return bookRepository.findAll();
+    }
+
     fun findAllByTitle(title: String) : Book {
         return bookRepository.findAllByTitle(title)
     }
+
+
 }
