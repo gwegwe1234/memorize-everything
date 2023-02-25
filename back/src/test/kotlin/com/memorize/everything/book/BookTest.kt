dafdc65688478ea2ee7565a9d48e06884719c703
gwegwe1234 <gwangtae5860@daum.net>
2023-02-25 20:13:19 +0900

    상용 url 수정 & 테스트 코드 프로토타입 추가

---
+++ back/src/test/kotlin/com/memorize/everything/book/BookTest.kt
@@ -0,0 +1,116 @@
+package com.memorize.everything.book
+
+import com.memorize.everything.common.BOOK_ALREADY_REGISTERED
+import com.memorize.everything.common.model.MemorizeException
+import com.memorize.everything.model.book.Book
+import com.memorize.everything.model.book.RequestBook
+import com.memorize.everything.repository.BookRepository
+import com.memorize.everything.service.BookService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+class BookTest {
+
+    @Mock
+    private lateinit var bookRepository: BookRepository
+
+    @InjectMocks
+    private lateinit var bookService: BookService
+
+    private val book1 = Book(1, "책 1", "책 1 내용", "경제")
+    private val book2 = Book(2, "책 2", "책 2 내용", "개발")
+    private val book3 = Book(3, "책 2", "책 3 내용", "소설")
+
+    private val books = listOf(book1, book2, book3)
+
+    @BeforeEach
+    fun setup() {
+        // TODO: 필요시 추가
+    }
+
+    @Test
+    @DisplayName("전체 책 조회")
+    fun fetch_all_book() {
+        //stubs
+        Mockito.`when`(bookRepository.findAll()).thenReturn(books)
+        Mockito.`when`(bookService.findAll())
+            .thenReturn(books)
+
+        //execute
+        val result = bookService.findAll()
+
+        //then
+        assertEquals(3, result?.size)
+    }
+
+    @Test
+    @DisplayName("아이디로 책 조회")
+    fun find_book_by_id() {
+        //given
+        val id = 2
+
+        //stubs
+        Mockito.`when`(bookRepository.findBookById(id)).thenReturn(book2)
+        Mockito.`when`(bookService.findBookById(id))
+            .thenReturn(book2)
+
+        //execute
+        val result = bookService.findBookById(2)
+
+        //then
+        assertEquals("책 2", result.title)
+    }
+
+    @Test
+    @DisplayName("책 입력 - 성공")
+    fun insert_book_success() {
+        //given
+        val requestBook = RequestBook("책 4", "책 4 내용", "경제")
+        val book = Book.from(requestBook)
+
+        //stubs
+        Mockito.`when`(bookRepository.findBookByTitle(requestBook.title)).thenReturn(null)
+        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+        Mockito.`when`(bookService.insertBook(requestBook))
+            .thenReturn(book)
+
+        //execute
+        val result = bookService.insertBook(requestBook)
+
+        //then
+        assertEquals("책 4", result.title)
+    }
+
+//    @Test
+//    @DisplayName("책 입력 - 실패")
+//    fun insert_book_fail() {
+//        //given
+//        val requestBook = RequestBook("책 4", "책 4 내용", "경제")
+//        val book = Book.from(requestBook)
+//
+//        //stubs
+//        Mockito.`when`(bookRepository.findBookByTitle(requestBook.title)).thenReturn(book)
+//        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+//        Mockito.`when`(bookService.insertBook(requestBook))
+//            .thenThrow(MemorizeException(BOOK_ALREADY_REGISTERED))
+//
+//        //execute
+//        val exception = assertThrows(MemorizeException::class.java) {
+//            bookService.insertBook(requestBook)
+//        }
+//
+//        //then
+//        assertEquals(BOOK_ALREADY_REGISTERED, exception.message)
+//    }
+}
